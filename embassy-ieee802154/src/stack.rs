e2ece8aa94de006d3b75722bc512094b6eef0b4f
Jeremy Dubrulle <jeremy.dubrulle@umons.ac.be>
2025-03-03 00:34:37 +0100

    embassy-ieee802154: Replace CSMA with new radio stack

--- embassy-ieee802154/src/stack.rs
+++ embassy-ieee802154/src/stack.rs
@@ -1,32 +1,26 @@
 use core::{cell::Cell, marker::PhantomData, panic};
 
-pub use dot15d4::csma::CsmaConfig;
 use dot15d4::{
-    csma::CsmaDevice,
-    phy::{
-        driver::FrameBuffer,
-        radio::{Radio, RadioFrameMut},
-    },
+    mac::{MacIndication, MacRequest},
+    phy::radio::{Radio, RadioFrameMut},
 };
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
 use embedded_hal_async::delay::DelayNs;
 use rand_core::RngCore;
 
-use crate::driver::{EmbassyDriver, Ieee802154Driver};
+use crate::driver::{EmbassyUpperLayer, Ieee802154Driver};
 
-pub struct CsmaStack<RADIO> {
-    config: CsmaConfig,
-    tx: Channel<NoopRawMutex, FrameBuffer, 1>,
-    rx: Channel<NoopRawMutex, FrameBuffer, 1>,
+pub struct RadioStack<RADIO> {
+    tx: Channel<NoopRawMutex, MacRequest, 1>,
+    rx: Channel<NoopRawMutex, MacIndication, 1>,
     radio: Cell<Option<RADIO>>,
     hardware_addr: [u8; 8],
 }
 
-impl<RADIO: Radio> CsmaStack<RADIO> {
-    pub fn new(radio: RADIO, config: CsmaConfig) -> Self {
+impl<RADIO: Radio> RadioStack<RADIO> {
+    pub fn new(radio: RADIO) -> Self {
         let hardware_addr = radio.ieee802154_address();
         Self {
-            config,
             tx: Channel::new(),
             rx: Channel::new(),
             radio: Cell::new(Some(radio)),
@@ -37,7 +31,7 @@ impl<RADIO: Radio> CsmaStack<RADIO> {
     pub fn driver(&self) -> Ieee802154Driver<'_, RADIO> {
         Ieee802154Driver {
             rx: None,
-            tx: FrameBuffer::default(),
+            tx: Default::default(),
             tx_channel: self.tx.sender(),
             rx_channel: self.rx.receiver(),
             hardware_addr: self.hardware_addr,
@@ -46,7 +40,7 @@ impl<RADIO: Radio> CsmaStack<RADIO> {
     }
 }
 
-impl<RADIO: Radio> CsmaStack<RADIO>
+impl<RADIO: Radio> RadioStack<RADIO>
 where
     for<'a> RADIO::RadioFrame<&'a mut [u8]>: RadioFrameMut<&'a mut [u8]>,
     for<'a> RADIO::TxToken<'a>: From<&'a mut [u8]>,
@@ -59,11 +53,11 @@ where
         let Some(radio) = self.radio.take() else {
             panic!("Stack already running");
         };
-        let driver = EmbassyDriver {
-            tx: self.tx.receiver(),
-            rx: self.rx.sender(),
+        let upper = EmbassyUpperLayer {
+            upper_tx: self.tx.receiver(),
+            upper_rx: self.rx.sender(),
         };
-        let mut csma = CsmaDevice::new(radio, rng, driver, timer, self.config);
-        csma.run().await
+        let mut device = dot15d4::Device::new(radio, rng, upper, timer);
+        device.run().await
     }
 }
