9e6df7938c9cdc44630405bf70bcee7ebfcc9ec9
Jeremy Dubrulle <jeremy.dubrulle@umons.ac.be>
2025-03-03 00:35:46 +0100

    embassy-ieee802154: update driver to handle MAC requests/indications instead of framebuffer

--- embassy-ieee802154/src/driver.rs
+++ embassy-ieee802154/src/driver.rs
@@ -1,8 +1,14 @@
 use core::marker::PhantomData;
 
-use dot15d4::phy::{
-    driver::{self, FrameBuffer},
-    radio::{self, Radio},
+use dot15d4::{
+    mac::{
+        Error as MacError, {MacIndication, MacRequest},
+    },
+    phy::{
+        radio::{self, Radio},
+        FrameBuffer,
+    },
+    upper::UpperLayer,
 };
 use embassy_net_driver::{Capabilities, HardwareAddress, LinkState};
 use embassy_sync::{
@@ -11,54 +17,54 @@ use embassy_sync::{
 };
 
 /// Driver for the `dot15d4` side
-pub(crate) struct EmbassyDriver<'a> {
-    pub(crate) tx: Receiver<'a, NoopRawMutex, FrameBuffer, 1>,
-    pub(crate) rx: Sender<'a, NoopRawMutex, FrameBuffer, 1>,
+pub(crate) struct EmbassyUpperLayer<'a> {
+    pub(crate) upper_tx: Receiver<'a, NoopRawMutex, MacRequest, 1>,
+    pub(crate) upper_rx: Sender<'a, NoopRawMutex, MacIndication, 1>,
 }
 
-impl driver::Driver for EmbassyDriver<'_> {
-    async fn transmit(&self) -> FrameBuffer {
-        self.tx.receive().await
+impl UpperLayer for EmbassyUpperLayer<'_> {
+    async fn mac_request(&self) -> MacRequest {
+        self.upper_tx.receive().await
     }
 
-    async fn received(&self, buffer: FrameBuffer) {
-        self.rx.send(buffer).await
+    async fn received_mac_indication(&self, indication: MacIndication) {
+        self.upper_rx.send(indication).await
     }
 
     /// Higher layers do not currently support error handling
-    async fn error(&self, error: driver::Error) {
+    async fn error(&self, error: MacError) {
         #[cfg(feature = "defmt")]
         defmt::error!("IEEE 802.15.4 Error: {}", error);
     }
 }
 
 pub struct Ieee802154Driver<'a, RADIO> {
-    pub(crate) rx: Option<FrameBuffer>,
-    pub(crate) tx: FrameBuffer,
-    pub(crate) tx_channel: Sender<'a, NoopRawMutex, FrameBuffer, 1>,
-    pub(crate) rx_channel: Receiver<'a, NoopRawMutex, FrameBuffer, 1>,
+    pub(crate) rx: Option<MacIndication>,
+    pub(crate) tx: MacRequest,
+    pub(crate) tx_channel: Sender<'a, NoopRawMutex, MacRequest, 1>,
+    pub(crate) rx_channel: Receiver<'a, NoopRawMutex, MacIndication, 1>,
     pub(crate) hardware_addr: [u8; 8],
     pub(crate) _radio: PhantomData<RADIO>,
 }
 
 impl<'ch, R: Radio> Ieee802154Driver<'ch, R> {
+    pub async fn submit_mac_request(&self, request: MacRequest) {
+        self.tx_channel.send(request).await;
+    }
+
     fn poll(&mut self, cx: &mut core::task::Context) {
         self.poll_transmit(cx);
         self.poll_receive(cx);
     }
 
     fn poll_receive(&mut self, cx: &mut core::task::Context) {
-        match &self.rx {
-            // Clean rx buffer can be removed.
-            Some(rx) if !rx.dirty => {
+        if let Some(MacIndication::McpsData(indication)) = &self.rx {
+            if !indication.buffer.dirty {
                 self.rx = None;
-            }
-            // Dirty rx buffer means we cannot poll the channel
-            Some(_rx) => {
+            } else {
+                // Dirty rx buffer means we cannot poll the channel
                 return;
             }
-            // No rx buffer means we can poll the channel
-            None => (),
         }
 
         if self.rx_channel.poll_ready_to_receive(cx).is_pending() {
@@ -75,12 +81,19 @@ impl<'ch, R: Radio> Ieee802154Driver<'ch, R> {
     }
 
     fn poll_transmit(&mut self, cx: &mut core::task::Context) {
-        if !self.tx.dirty {
-            return;
-        }
+        match &self.tx {
+            MacRequest::McpsDataRequest(request) => {
+                if !request.buffer.dirty {
+                    return;
+                }
 
-        if self.tx_channel.poll_ready_to_send(cx).is_pending() {
-            return;
+                if self.tx_channel.poll_ready_to_send(cx).is_pending() {
+                    return;
+                }
+            }
+            _ => {
+                return;
+            }
         }
 
         let msg = core::mem::take(&mut self.tx);
@@ -102,25 +115,28 @@ where
     for<'a> RADIO::TxToken<'a>: From<&'a mut [u8]>,
     for<'b> RADIO::RxToken<'b>: From<&'b mut [u8]>,
 {
-    type RxToken<'a> = RxToken<'a, RADIO::RxToken<'a>>
+    type RxToken<'a>
+        = RxToken<'a, RADIO::RxToken<'a>>
     where
         Self: 'a;
 
-    type TxToken<'a> = TxToken<'a, RADIO::TxToken<'a>>
+    type TxToken<'a>
+        = TxToken<'a, RADIO::TxToken<'a>>
     where
         Self: 'a;
 
     fn receive(&mut self, cx: &mut core::task::Context) -> Option<(Self::RxToken<'_>, Self::TxToken<'_>)> {
         self.poll(cx);
-
-        match (&mut self.rx, self.tx.dirty) {
-            (Some(ref mut rx), false) if rx.dirty => {
+        match (&mut self.rx, &mut self.tx) {
+            (Some(MacIndication::McpsData(indication)), MacRequest::McpsDataRequest(request))
+                if !request.buffer.dirty && indication.buffer.dirty =>
+            {
                 let rx = RxToken {
-                    frame: rx,
+                    frame: &mut indication.buffer,
                     _inner_token: PhantomData,
                 };
                 let tx = TxToken {
-                    frame: &mut self.tx,
+                    frame: &mut request.buffer,
                     _inner_token: PhantomData,
                 };
                 Some((rx, tx))
@@ -132,14 +148,18 @@ where
     fn transmit(&mut self, cx: &mut core::task::Context) -> Option<Self::TxToken<'_>> {
         self.poll(cx);
 
-        if self.tx.dirty {
-            return None;
+        match &mut self.tx {
+            MacRequest::McpsDataRequest(request) => {
+                if request.buffer.dirty {
+                    return None;
+                }
+                Some(TxToken {
+                    frame: &mut request.buffer,
+                    _inner_token: PhantomData,
+                })
+            }
+            _ => None,
         }
-
-        Some(TxToken {
-            frame: &mut self.tx,
-            _inner_token: PhantomData,
-        })
     }
 
     fn link_state(&mut self, cx: &mut core::task::Context) -> LinkState {
--- embassy-ieee802154/src/lib.rs
+++ embassy-ieee802154/src/lib.rs
@@ -1,8 +1,9 @@
 #![no_std]
 
 pub use dot15d4::frame;
-pub use dot15d4::phy::driver::FrameBuffer;
+pub use dot15d4::mac::command::{MacIndication, MacRequest};
+pub use dot15d4::phy::FrameBuffer;
 pub use dot15d4::phy::{config, radio};
 
-pub mod csma;
 pub mod driver;
+pub mod stack;
